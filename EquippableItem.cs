ba28bb7f3fe52ba46f24f321457177d466dd177a
Clausenjm <72426840+Clausenjm@users.noreply.github.com>
2020-10-10 17:30:38 -0500

    Add files via upload
    This was put together from YouTube video's and internet instructions

---
+++ EquippableItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum EquipmentType
+{
+    Helmet,
+    Chest,
+    Gloves,
+    Boots,
+    Weapon1,
+    Weapon2,
+    Accessory1,
+    Accessory2,
+
+}
+[CreateAssetMenu]
+
+public class EquippableItem : Item
+{
+    public int StrengthBonus;
+    public int AgilityBonus;
+    public int IntelligenceBonus;
+    public int VitalityBonus;
+    [Space]
+    public int StrengthPercentBonus;
+    public int AgilityPercentBonus;
+    public int IntelligencePercentBonus;
+    public int VitalityPercentBonus;
+    [Space]
+
+    public EquipmentType EquipmentType;
+
+}
---
+++ HideInventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideInventory : MonoBehaviour
+{
+    private Canvas test;
+    private void Start()
+    {
+        test = GetComponent<Canvas>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            test.enabled = !test.enabled;
+
+        } 
+    }
+    
+}
